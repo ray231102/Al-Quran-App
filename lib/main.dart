6a8196a76ee75d330eeff160dc8450a401a369a9
khay_ray <farrashaidar005@gmail.com>
2021-01-27 12:23:25 +0700

    done

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:alquran_app/widget/quran_ayat_model.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import './screen/quran_list.dart';
+import './screen/quran_detail.dart';
+
+import './models/quran_model.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider.value(
+          value: QuranData(),
+        ),
+        ChangeNotifierProvider.value(
+          value: QuranAyat(),
+        )
+      ],
+      child: MaterialApp(
+        title: 'DaengwebID',
+        theme: ThemeData(
+          primarySwatch: Colors.pink,
+        ),
+        home: QuranList(),
+        routes: {
+          '/detail': (ctx) => QuranDetail(),
+        },
+      ),
+    );
+  }
+}
